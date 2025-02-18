4e9bf5da20a12e4a8976829e60098ea4be7cdfa2
Kate <159571202+turchynska@users.noreply.github.com>
2025-02-18 11:01:47 -0500

    Initial commit

---
+++ java.js
@@ -0,0 +1,133 @@
+// let name = "Joe";
+// let surName = "Doe";
+// let age = 45;
+// age = 30;
+// console.log(age)
+// console.log(name);
+// console.log(surName);
+
+// const pi = 3.14;
+// pi = 4;
+// console.lo9g(pi)
+
+// let result = 3 + 2 - 76 * (1 + 1);
+// console.log(result)
+
+// console.log(23 + 97);
+// console.log((4 + 6 + 9) / 77);
+
+// let a = 10;
+// console.log(a)
+// a = 45;
+// console.log(a)
+
+// let b = 7 * a;
+// console.log(b)
+
+// const max = 57;
+// const actual = max - 13;
+// const percentage = actual / max;
+// console.log(percentage);
+
+// let counter = 3;
+// ++counter
+// console.log(counter)
+
+// let str = "HEllo";
+// let str2 = 'Single quotes are ok too';
+// let srt3 = `Here is ${str}`
+// console.log(srt3)
+
+// let isBigger = 5 > 1;
+// console.log(isBigger)
+
+//STRING
+// const string = "The revolution will not be televised";
+// console.log(string)
+// const badString = string;
+// console.log(badString)
+
+// const name = "Michael";
+// const greeting = `Hello ${name}`;
+// console.log(greeting)
+
+// const one = "Hello";
+// const two = " how are you?";
+// const joined = `${one}${two}`;//concatenation
+// console.log(joined)
+
+
+// const btn = document.querySelector("button");
+// function greet() {
+//     const name = prompt("What is your name?");
+//     const greeting = document.querySelector("#greeting");
+//     greeting.textContent = `Hello ${name}, nice to meet you`
+// }
+
+// btn.addEventListener("click", greet);
+
+//Concatenation +
+// const name = "Chris";
+// const greeting = "Hello";
+// console.log(greeting + " " + name);
+
+// const song = "Forever young";
+// const score = 9;
+// const highestScore = 10;
+// const output = `I like the song ${song}. I gave it a score of ${(score / highestScore) * 100}`;
+// console.log(output)
+
+// const bigmouth = "I've got no right to take my place…";
+//  console.log(bigmouth)
+
+
+//NUMBERS + STRING
+// const name = "Front ";
+// const number = 242;
+// console.log(name + number);
+
+//number()
+// const myString = "1234";
+// const myNumber = Number(myString);
+// console.log(typeof myNumber)
+
+//string
+// const myNum2 = 123;
+// const myStr2 = String(myNum2);
+// console.log(typeof myStr2 )
+
+//length
+// let text = "WREYIDNDONQ";
+// let length = text.length;
+// console.log(length)
+
+//charCodeAt()
+// let text = "Hello World";
+// let char = text.charCodeAt(0)
+// console.log(char)
+
+//slice
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-7);
+// console.log(part)
+
+//concat
+// let text1 = "Hello";
+// let text2 = "World!";
+// let text3 = text1.concat(" ", text2);
+// console.log(text3)
+
+//padstart
+// let text = "5";
+// let padded = text.padStart(4, "0");
+// console.log(padded)
+
+//Boolean
+let a = 0;
+console.log(Boolean(a))
+
+let b = "0";
+console.log(Boolean(b))
+
+console.log(a == b)
+
